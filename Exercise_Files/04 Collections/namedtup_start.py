483775dd4f9484e04fdce22f4b6dd64f76241518
Behrokh Farzad <behrokhfarzad@gmail.com>
2021-02-01 10:40:27 -0800

    collections.namedtuple(typename, fieldname) returns a new tuple subclass

--- Exercise_Files/04 Collections/namedtup_start.py
+++ Exercise_Files/04 Collections/namedtup_start.py
@@ -5,10 +5,16 @@ import collections
 
 def main():
     # TODO: create a Point namedtuple
+    Point = collections.namedtuple('point', 'x y')
+    p1 = Point(10,20)
+    p2 = Point(30,40)
+    print(p1)
+    print(p2)
 
     # TODO: use _replace to create a new instance
-    pass
-
+    p1 = p1._replace(x=100)
+    print(p1)
+    
 
 if __name__ == "__main__":
     main()
