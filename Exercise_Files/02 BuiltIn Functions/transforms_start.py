b23aec1ed93437b0adac8e22b3ddeb47a8e2e740
Behrokh Farzad <behrokhfarzad@gmail.com>
2021-02-01 10:07:42 -0800

    examples of filter, map, zip, sorted functions

--- Exercise_Files/02 BuiltIn Functions/transforms_start.py
+++ Exercise_Files/02 BuiltIn Functions/transforms_start.py
@@ -2,19 +2,30 @@
 
 
 def filterFunc(x):
-    pass
+     if x % 2 == 0:
+         return True
+     else:
+         return False
 
 
 def filterFunc2(x):
-    pass
+    return True if x.isupper() else False
 
 
 def squareFunc(x):
-    pass
+    return x**2
 
 
 def toGrade(x):
-    pass
+    if x >= 90:
+        return 'A'
+    elif 80 <= x < 90:
+        return 'B'
+    elif 70 <= x < 80:
+        return 'C'
+    elif 60 <= x < 70:
+        return 'D'
+    return 'F'
 
 
 def main():
@@ -24,13 +35,22 @@ def main():
     grades = (81, 89, 94, 78, 61, 66, 99, 74)
 
     # TODO: use filter to remove items from a list
-
+    # filter(function, iterable) 
+    odds = list(filter(filterFunc, nums))
+    print(odds)
+    
     # TODO: use filter on non-numeric sequence
+    ch = list(filter(filterFunc2, chars))
+    print(ch)
 
     # TODO: use map to create a new sequence of values
+    sq = list(map(squareFunc, nums))
+    print(sq)
 
     # TODO: use sorted and map to change numbers to grades
-
+    grades = sorted(grades)
+    newList = list(map(toGrade, grades))
+    print(newList)
 
 if __name__ == "__main__":
     main()
