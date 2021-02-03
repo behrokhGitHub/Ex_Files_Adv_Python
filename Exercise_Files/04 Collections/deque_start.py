55bc0169eb8d2e524f48394c36912e0f1351db05
Behrokh Farzad <behrokhfarzad@gmail.com>
2021-02-02 16:21:16 -0800

    deque,Memory efficient for operations on both ends

--- Exercise_Files/04 Collections/deque_start.py
+++ Exercise_Files/04 Collections/deque_start.py
@@ -1,21 +1,32 @@
 # deque objects are like double-ended queues
 
-import collections
+import collections 
 import string
 
 
 def main():
     
     # TODO: initialize a deque with lowercase letters
+    d = collections.deque(string.ascii_lowercase)
 
     # TODO: deques support the len() function
-
+    print(len(d))
+    
     # TODO: deques can be iterated over
+    for c in d:
+        print(c, end=',')
+    print()
 
     # TODO: manipulate items from either end
-
+    d.pop()
+    d.popleft()
+    d.append(2)
+    d.appendleft(1)
+    print(d)
+    print()
     # TODO: rotate the deque
-
+    d.rotate(10)
+    print(d)
 
 if __name__ == "__main__":
     main()
